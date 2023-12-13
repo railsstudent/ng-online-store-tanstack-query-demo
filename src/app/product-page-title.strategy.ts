a6bd1759a043a43145ebce9ed67a480b80a93346
Connie Leung <cleung334@gmail.com>
2023-12-14 00:57:53 +0800

    feat: update title to display product name

--- src/app/product-page-title.strategy.ts
+++ src/app/product-page-title.strategy.ts
@@ -1,30 +1,27 @@
-import { DestroyRef, inject, Injectable } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { inject, Injectable, OnDestroy } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
-import { map, tap } from 'rxjs';
+import { map, Subscription } from 'rxjs';
 import { ProductService } from './products/services/product.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ProductPageTitleStrategy extends TitleStrategy {
   title = inject(Title);
   productService = inject(ProductService);
-  destroyRef$ = inject(DestroyRef);
+  subscription: Subscription | null = null;
   
   updateTitle(snapshot: RouterStateSnapshot): void {
+    this.subscription?.unsubscribe();
+
     const customTitle = this.buildTitle(snapshot) || '';
     const productId = snapshot.root.firstChild?.params['id'] || '';
     if (productId) {
-        this.productService.getProduct(productId).result$
+        this.subscription = this.productService.getProduct(productId).result$
           .pipe(
             map((product) => product.data?.title || ''),
             map((productTitle) => `Product - ${productTitle}`),
-            tap((pageTitle) => this.title.setTitle(pageTitle)),
-            takeUntilDestroyed(this.destroyRef$),
           )
-          .subscribe();
+          .subscribe((pageTitle) => this.title.setTitle(pageTitle));
     } else {
       this.title.setTitle(customTitle);
     }
