c25e95d38555570ba5dfc2ef8e9e373c013b17bf
Connie Leung <cleung334@gmail.com>
2023-12-14 00:48:04 +0800

    chore: delete blank line

--- src/app/product-page-title.strategy.ts
+++ src/app/product-page-title.strategy.ts
@@ -25,7 +25,6 @@ export class ProductPageTitleStrategy extends TitleStrategy {
             takeUntilDestroyed(this.destroyRef$),
           )
           .subscribe();
-
     } else {
       this.title.setTitle(customTitle);
     }
