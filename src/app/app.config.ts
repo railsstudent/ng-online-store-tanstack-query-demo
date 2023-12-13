2db5ec7bcaf91232cbbfe58a5f4164b1c2c2d9b9
Connie Leung <cleung334@gmail.com>
2023-12-14 00:38:37 +0800

    feat: title strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { TitleStrategy, provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
+import { ProductPageTitleStrategy } from './product-page-title.strategy';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    provideRouter(routes, withComponentInputBinding())
+    provideRouter(routes, withComponentInputBinding()),
+    {
+      provide: TitleStrategy,
+      useClass: ProductPageTitleStrategy,
+    }
   ]
 };
---
+++ src/app/product-page-title.strategy.ts
@@ -0,0 +1,33 @@
+import { DestroyRef, inject, Injectable } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Title } from '@angular/platform-browser';
+import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
+import { map, tap } from 'rxjs';
+import { ProductService } from './products/services/product.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductPageTitleStrategy extends TitleStrategy {
+  title = inject(Title);
+  productService = inject(ProductService);
+  destroyRef$ = inject(DestroyRef);
+  
+  updateTitle(snapshot: RouterStateSnapshot): void {
+    const customTitle = this.buildTitle(snapshot) || '';
+    const productId = snapshot.root.firstChild?.params['id'] || '';
+    if (productId) {
+        this.productService.getProduct(productId).result$
+          .pipe(
+            map((product) => product.data?.title || ''),
+            map((productTitle) => `Product - ${productTitle}`),
+            tap((pageTitle) => this.title.setTitle(pageTitle)),
+            takeUntilDestroyed(this.destroyRef$),
+          )
+          .subscribe();
+
+    } else {
+      this.title.setTitle(customTitle);
+    }
+  }
+}
